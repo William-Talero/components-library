8a9343d4e163efbc0f89b2f9b2084c5928575eb8
William Talero <andresyjp2204@gmail.com>
2024-08-15 11:47:01 -0500

    Was update the button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,18 @@
 import styled from "styled-components";
 
 const Button = styled.button`
+  width: 100%;
+  background-color: #4caf50;
+  border: none;
   color: white;
-  background-color: blue;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 1rem 0;
+  cursor: pointer;
+  border-radius: 1rem;
 `;
 
 export { Button };
