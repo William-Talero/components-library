8f25931e062538445e6f3bf5b500624c7e9d4e61
William Talero <andresyjp2204@gmail.com>
2024-08-15 13:00:57 -0500

    Button Styles updated to accept different sizes

--- dist/components/Button/Button.d.ts
+++ dist/components/Button/Button.d.ts
@@ -1,2 +1,3 @@
-declare const Button: import("styled-components/dist/types").IStyledComponentBase<"web", import("styled-components").FastOmit<import("react").DetailedHTMLProps<import("react").ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, never>> & string;
+import { ButtonProps } from "./IButton";
+declare const Button: import("styled-components/dist/types").IStyledComponentBase<"web", import("styled-components/dist/types").Substitute<import("react").DetailedHTMLProps<import("react").ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, ButtonProps>> & string;
 export { Button };
