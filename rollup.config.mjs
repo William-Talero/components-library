a8c3c15702a5ef7215144b8c96f715ed5b8a3a05
William Talero <andresyjp2204@gmail.com>
2024-09-04 12:31:03 -0500

    Was exclude the css generate

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import typescript from "rollup-plugin-typescript2";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import sass from "rollup-plugin-sass";
+import postcss from "rollup-plugin-postcss";
 import alias from "@rollup/plugin-alias";
 import pkg from "./package.json" with { type: "json" };
 
@@ -40,9 +40,19 @@ export default {
     alias({
       entries: [{ find: "@", replacement: "./src" }],
     }),
-    sass({
-      output: true,
+    postcss({
+      extensions: [".css", ".scss"],
+      extract: false,
+      minimize: true,
+      use: [
+        [
+          "sass",
+          {
+            includePaths: ["./src/**/*.scss"],
+          },
+        ],
+      ],
     }),
   ],
-  external: ["react", "react-dom", "styled-components"],
+  external: ["react", "react-dom"],
 };
