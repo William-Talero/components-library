59e91dd0bd6641538a13cf81e25bd6e09593870d
William Talero <andresyjp2204@gmail.com>
2024-09-04 11:59:25 -0500

    Come back to sass

--- dist/components/Button/stories/Button.stories.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { ButtonProps } from "@/components/Button/IButton";
-declare const meta: Meta<ButtonProps>;
-export default meta;
-type Story = StoryObj<ButtonProps>;
-export declare const Primary: Story;
-export declare const Secondary: Story;
-export declare const Disabled: Story;
--- dist/components/Button/stories/ButtonWithIcon.stories.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { ButtonWithIconProps } from "@/components/Button/IButton";
-declare const meta: Meta<ButtonWithIconProps>;
-export default meta;
-type Story = StoryObj<ButtonWithIconProps>;
-export declare const Primary: Story;
-export declare const Secondary: Story;
-export declare const Disabled: Story;
