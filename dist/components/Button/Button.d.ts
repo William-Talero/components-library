68ae17867aee7ef63f2ccbae9264092345e299b2
William Talero <andresyjp2204@gmail.com>
2024-09-04 11:17:13 -0500

    Was recover styled-componets

--- dist/components/Button/Button.d.ts
+++ dist/components/Button/Button.d.ts
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { ButtonWithIconProps, ButtonProps } from "./IButton";
-import "../../styles.scss";
-declare const Button: (data: ButtonProps) => React.JSX.Element;
 declare const ButtonWithIcon: (data: ButtonWithIconProps) => React.JSX.Element;
+declare const Button: (data: ButtonProps) => React.JSX.Element;
 declare const OrangeButton: (data: ButtonProps) => React.JSX.Element;
 export { Button, ButtonWithIcon, OrangeButton };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,40 +6,26 @@ import {
   OrangeButtonElement,
 } from "./Button.styles";
 import Icon from "../Icon/Icons";
-import "../../styles.scss";
 
-const Button = (data: ButtonProps) => {
-  const { children, $type, $size, $m, ...rest } = data;
-  const className = `button button-${$type} button-${$size} button-${$m}`;
-
+const ButtonWithIcon = (data: ButtonWithIconProps) => {
+  const { $size, $icon: icon, children } = data;
   return (
-    <button className={className} {...rest}>
-      {children}
-    </button>
+    <Button {...data}>
+      <Icon $name={icon} $w="20%" />
+      <ButtonContent>{children}</ButtonContent>
+      {$size == "lg" && <Icon $name={icon} $w="20%" />}
+    </Button>
   );
 };
 
-const ButtonWithIcon = (data: ButtonWithIconProps) => {
-  const { children, $type, $size, $m, ...rest } = data;
-  const classNames = `button button-${$type} button-${$size} button-${$m}`;
-
-  return (
-    <button className={classNames} {...rest}>
-      <Icon $name="plus" $w="10%" />
-      <div style={{ width: "100%" }}>{children}</div>
-      {$size === "lg" && <Icon $name="plus" $w="10%" />}
-    </button>
-  );
+const Button = (data: ButtonProps) => {
+  const { children } = data;
+  return <ButtonElement {...data}>{children}</ButtonElement>;
 };
 
 const OrangeButton = (data: ButtonProps) => {
-  const { children, $type, $size, $m, ...rest } = data;
-  const classNames = `button button-${$type} button-${$size} button-${$m}`;
-
-  return (
-    <button className={classNames} {...rest}>
-      {children}
-    </button>
-  );
+  const { children } = data;
+  return <OrangeButtonElement {...data}>{children}</OrangeButtonElement>;
 };
+
 export { Button, ButtonWithIcon, OrangeButton };
